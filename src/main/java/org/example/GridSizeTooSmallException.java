f45370874c36d427627e1e778281f0e117c2ef49
Aizen648 <you@example.com>
2023-02-18 14:13:21 +0100

    - My own Exception

--- src/main/java/org/example/GridSizeTooSmallException.java
+++ src/main/java/org/example/GridSizeTooSmallException.java
@@ -2,6 +2,6 @@ package org.example;
 
 public class GridSizeTooSmallException extends IllegalArgumentException{
     public GridSizeTooSmallException(){
-        super("Grid Size cannot be less that 2");
+        super("Grid Size cannot be less that 2.");
     }
 }
